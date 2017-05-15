bbec7e55aae71322797c36dfb65dbd81f5116c10
AUTplayed <fips.hem@gmail.com>
2017-05-16 00:17:40 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var Snoo = require("snoocore");
+require("dotenv").config();
+var env = process.env;
+
+var reddit = new Snoo({
+    userAgent: env.useragent,
+    oauth:{
+        type:'script',
+        key: env.redditclientkey,
+        secret: env.redditsecret,
+        username: env.reddituser,
+        password: env.redditpw,
+        scope: ['identity','read']
+    }
+});
+
+reddit('/api/v1/me').get().then(function(result) {
+  console.log(result); 
+});
