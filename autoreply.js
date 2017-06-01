5459361c2a27920cb4dac36dcc4d63241495fd5e
AUTplayed <fips.hem@gmail.com>
2017-06-01 12:31:37 +0200

    added more autoreply choices

--- autoreply.js
+++ autoreply.js
@@ -5,9 +5,15 @@ module.exports = [{
     key:["the best"],
     reply:["https://i.imgur.com/Yzu3aOZ.gif"]
 },{
-    key:["hello","hi"],
+    key:["hello","hi","hey"],
     reply:["hey, what's up?","yo, what's up?"]
 },{
     key:["why"],
     reply:["[Because](https://github.com/AUTplayed/imguralbumbot/blob/master/README.md)"]
+},{
+    key:["thank you","thanks"],
+    reply:["no problem","you're welcome","my pleasure"]
+},{
+    key:["i changed the","i updated the"],
+    reply:["thanks","thank you"]
 }];
