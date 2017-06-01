d523f65c1062c64bb0de48ce34e9ffa9b2151c2e
shitposting <hypnozooidreddit@gmail.com>
2017-06-01 11:04:18 -0400

    Fix links in autoreplies (#6)
    * remove parentheses breaking links in replies
    
    * Add parentheses for replies with multiple words

--- autoreply.js
+++ autoreply.js
@@ -1,19 +1,19 @@
 module.exports = [{
-    key:["<3","love you"],
+    key:["<3","(love you)"],
     reply:["<3"]
 },{
     key:["the best"],
     reply:["https://i.imgur.com/Yzu3aOZ.gif"]
 },{
     key:["hello","hi","hey"],
-    reply:["hey, what's up?","yo, what's up?"]
+    reply:["(hey, what's up?)","(yo, what's up?)"]
 },{
     key:["why"],
     reply:["Please read the link called 'why' in my comment"]
 },{
     key:["thank you","thanks"],
-    reply:["no problem","you're welcome","my pleasure"]
+    reply:["(no problem)","(you're welcome)","(my pleasure)"]
 },{
     key:["changed the link","updated the link"],
-    reply:["thanks","thank you"]
+    reply:["thanks","(thank you)"]
 }];
