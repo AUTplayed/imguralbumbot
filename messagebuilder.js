ff914d66744065145dcc368c5bfe34b03761f611
AUTplayed <fips.hem@gmail.com>
2017-06-19 11:23:58 +0200

    added state of imgur footer and made footer smaller

--- messagebuilder.js
+++ messagebuilder.js
@@ -1,8 +1,9 @@
 const header = "^(Hi, I'm a bot for linking direct images of albums with only 1 image)";
-var footer = "\n\n^[Source](https://github.com/AUTplayed/imguralbumbot) ";
-footer += "^| ^[Why?](https://github.com/AUTplayed/imguralbumbot/blob/master/README.md) ";
-footer += "^| ^[Creator](https://np.reddit.com/user/AUTplayed/) ";
-footer += "^| ^[ignoreme](https://np.reddit.com/message/compose/?to=imguralbumbot&subject=ignoreme&message=ignoreme) ";
+var footer = "\n\n^^[Source](https://github.com/AUTplayed/imguralbumbot) ";
+footer += "^^| ^^[Why?](https://github.com/AUTplayed/imguralbumbot/blob/master/README.md) ";
+footer += "^^| ^^[Creator](https://np.reddit.com/user/AUTplayed/) ";
+footer += "^^| ^^[state_of_imgur](https://np.reddit.com/user/imguralbumbot/comments/6i1huv/imgur_has_gone_to_shit/)";
+footer += "^^| ^^[ignoreme](https://np.reddit.com/message/compose/?to=imguralbumbot&subject=ignoreme&message=ignoreme) ";
 
 
 
