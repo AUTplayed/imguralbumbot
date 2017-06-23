64df61fb3314e929f069b3de0bcd7bce7541bb5b
AUTplayed <fips.hem@gmail.com>
2017-06-23 13:02:44 +0200

    ups, fixed no reply

--- messagebuilder.js
+++ messagebuilder.js
@@ -32,4 +32,5 @@ function modremove(post){
     var msg =  "Your submission was removed because you linked an album with only one image instead of the ";
     msg+= "[direct link]("+post.direct[0]+") \n\n";
     msg+="Learn how to get a direct link [here](https://github.com/AUTplayed/imguralbumbot/blob/master/README.md#what-can-i-do-against-it)";
+    return msg;
 }
